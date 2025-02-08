bb7944fbb37d1d692256f9e9d76bfd748f17d01d
Gisele Granato <gfggiza@gmail.com>
2025-02-08 17:32:10 +0000

    Remove unused services and related code for improved project cleanliness

--- lib/services/watchmode_client.dart
+++
@@ -1,7 +0,0 @@
-import 'package:assignment/api/watchmode_api.dart';
-import 'package:assignment/configuration/config.dart';
-
-final watchmodeClient = createWatchmodeApi(
-  baseUrl: config.watchmodeBaseUrl,
-  apiKey: config.watchmodeApiKey,
-);
